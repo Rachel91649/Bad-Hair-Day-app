becdb6b621e192c48d25f32a15dc5f91ee221fe4
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-07-08 17:08:08 -0400

    added psCode to hair controller

--- controllers/hair.js
+++ controllers/hair.js
@@ -1,1 +1,33 @@
-hair.js
+// =============================
+//	REQUIREMENTS
+// =============================
+var express = require('express');
+var mongoose = require('mongoose');
+var db = process.env.MONGODB_URI || "mongodb://localhost/bad-hair-day";
+var router = express.Router();
+var Good = require('../models/goodday.js');
+var Bad = require('../models/badday.js');
+
+
+// =============================
+//	ROUTES
+// =============================
+//need route to loop through Good hair collection and return randomly selected image if the humidity is <= a specified percentage and display on screen(ajax call on front end will handle that)
+//need route to loop through Bad hair collection and return randomly selected image if the humidity is >= a specified percentage and display on screen(ajax call on front end will handle that)
+
+
+//need a route to create new good and bad hair day images
+//they can be created via post man
+//need a route to update an existing good/bad hair day image
+//can be done via postman
+//need a route to delete an existing good/bad hair day image
+//can be done via postman???
+
+
+// =============================
+//	API CALL
+// =============================
+
+//need route to weather api
+
+module.exports = router;
