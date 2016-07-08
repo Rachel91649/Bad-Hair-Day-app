da11de148d73e1e24b62bc44b696bad8850ee722
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-07-08 17:38:01 -0400

    more psCode

--- public/scripts.js
+++ public/scripts.js
@@ -3,32 +3,41 @@ console.log("I'm going to make React Love me...");
 // ===========================================
 // MAIN COMPONENT - THE KEEPER OF LOST SOULS
 // ===========================================
+// will hold all the state
+
 
 // ===========================================
 // SEARCH COMPONENT
 // ===========================================
-
+//changes state
 
 // ===========================================
 // WEATHER DISPLAY COMPONENT
 // ===========================================
+// just renders..??
+
 
 // ===========================================
 // WEATHER SEARCH RESULT COMPONENT
 // ===========================================
+// just renders
+
 
 // ===========================================
 // IMAGE GRAB QUERY COMPONENT
 // ===========================================
+//changes state??? not sure yet
+
 
 // ===========================================
 // IMAGE DISPLAY COMPONENT
 // ===========================================
+// just renders
 
 // ===========================================
 //	RESET BUTTON COMPONENT
 // ===========================================
-
+// on click this will reset the entire page back to it's original state
 
 
 // ======================================
